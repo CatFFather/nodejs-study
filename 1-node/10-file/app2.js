f43e33763efac93cf2e3603f84fb7d449096263e
unknown <jh2000lee@nate.com>
2022-01-04 23:11:27 +0700

    [MOD]10-file 수정

--- 1-node/10-file/app2.js
+++ 1-node/10-file/app2.js
@@ -1,3 +1,4 @@
+// require할 때 fs의 promises 형태로 바로 가져옴
 const fs = require("fs").promises;
 
 // read a file(파일 읽기)
@@ -25,10 +26,19 @@ fs.writeFile("./file.txt", "Yo!, Dream Coders! :) ") //
 
 // 기존 파일에 내용을 추가 하고싶으면 appendFile을 사용
 fs.appendFile("./file.txt", "append!, Dream Coders! :) ") //
-  .then(() => {})
+  .then(() => {
+    // 비동기라서 파일쓰기가 완료 된 후에 copy 해줘야함
+    fs.copyFile("./file.txt", "./file-copy.txt");
+  })
   .catch((error) => {
     console.log(error);
   });
+
+/*
+  ※ 비동기는 순차적으로 될 수도 있고 안될 수도 있다.
+  순서대로 코드를 작성했지만 모든 것이 promises이므로 비동기적으로 처리되기 때문에 순서가 보장되지 않는다.
+  순서가 중요한 경우라면 then 안에서 해당하는 promises가 다 수행된 다음 특정한 일을 해야 한다.
+*/
 
 // copy (파일 복사)
 fs.copyFile("./copyTest.txt", "./copyTest-copy.txt") //
@@ -46,6 +56,7 @@ fs.mkdir("sub-folder") //
 // 해당하는 경로에 있는 모든 파일들의 이름을 읽어올 수 있음(폴더 이름도 출력됨)
 fs.readdir("./") //
   .then((data) => {
+    // string 배열 형태로 return 됨 (Promise<string[]>)
     console.log(data);
   })
   .catch((error) => {
