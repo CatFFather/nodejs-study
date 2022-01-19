bae690d0bb57b20e2672e619a86e366d5f3ed950
unknown <jh2000lee@nate.com>
2022-01-19 22:18:12 +0700

    [FIX]EventEmitter 개선 코드 반영

--- 1-node/13-event/loggerError.js
+++ 1-node/13-event/loggerError.js
@@ -8,3 +8,17 @@ function log(callback) {
 }
 
 module.exports.log = log;
+
+// // 개선 코드
+
+// const EventEmitter = require("events");
+// const emitter = new EventEmitter();
+
+// function log(callback) {
+//   emitter.emit("log", "started...");
+//   callback();
+//   emitter.emit("log", "ended!");
+// }
+
+// module.exports.emitter = emitter;
+// module.exports.log = log;
--- 1-node/13-event/mainError.js
+++ 1-node/13-event/mainError.js
@@ -12,3 +12,17 @@ logger.log(() => {
 // 누가 event를 emit()하는 곳이 없다.
 // 듣고있는 event(main.js의 emitter)와 다른 파일(loggerError.js의 emitter)에서 emit하고있는 emitter는 전혀 다른 객체임
 // event가 발생하고 해당 이벤트를 듣기 위해서는 동일한 EventEmitter객체를 사용 해야함
+
+// // 개선 코드
+
+// const logger = require("./loggerError");
+// const EventEmitter = require("events");
+// const emitter = new EventEmitter();
+
+// logger.emitter.on("log", (event) => {
+//   console.log(event);
+// });
+
+// logger.log(() => {
+//   console.log("..... doing something!");
+// });
