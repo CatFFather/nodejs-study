23bab81cc9680b9e0f5d785dadef3461a0dde264
unknown <jh2000lee@nate.com>
2022-06-06 21:41:15 +0700

    [ADD]9.4 템플릿 엔진 EJS 사용해보기 (서버사이드 렌더링)

---
+++ 5-node-server/2-template.js
@@ -0,0 +1,33 @@
+const http = require("http");
+const fs = require("fs");
+const ejs = require("ejs");
+
+const name = "CatFFather";
+const courses = [
+  { name: "HTML" },
+  { name: "CSS" },
+  { name: "JS" },
+  { name: "Node" },
+  { name: "Front-end" },
+];
+
+const server = http
+  .createServer((req, res) => {
+    const url = req.url;
+    res.setHeader("Content-Type", "text/html");
+    if (url === "/") {
+      ejs
+        .renderFile("./template/index.ejs", { name })
+        .then((data) => res.end(data));
+    } else if (url === "/courses") {
+      ejs
+        .renderFile("./template/courses.ejs", { courses })
+        .then((data) => res.end(data));
+    } else {
+      console.log("not-found");
+      ejs
+        .renderFile("./template/not-found.ejs", { name })
+        .then((data) => res.end(data));
+    }
+  })
+  .listen(8080);
