3ec82c7fc1b9f521dbcc79b6682349a97427c5a2
이재홍사원 <jh2000lee@nate.com>
2022-08-11 10:25:19 +0900

    [ADD]11.11 유용한 내부 미들웨어들

--- 6-express/app-route.js
+++ 6-express/app-route.js
@@ -4,7 +4,22 @@ import userRouter from './router/user.js';
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // REST API --> Body Parsing
+app.use(express.urlencoded({ extended: false })); // HTML Form --> Body Parsing
+// HTML Form 에서 submit 시 자동으로 reqest 발생 그 때 전달된 HTML 에서 만든 데이터를 body에 자동으로 parsing 해준다.
+
+const options = {
+    dotfiles: 'ignore',
+    etag: false,
+    index: false,
+    maxAge: 'id',
+    redirect: false,
+    setHeaders: function (res, path, stat) {
+        res.set('x-timestamp', Date.now());
+    },
+};
+
+app.use(express.static('public', options));
 app.use('/posts', postRouter);
 app.use('/users', userRouter);
 
